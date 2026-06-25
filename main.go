d70ba9a5d1e95ba1be29b1308fb34ce5838478a7
Laith Taher <lotaher04@gmail.com>
2026-06-25 23:23:42 +0300

    added digest retrieval route

--- main.go
+++ main.go
@@ -4,10 +4,22 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"path/filepath"
 	"strconv"
 )
 
 const PORT = 6769
+
+// Helpers
+
+func getBlob(digest string) (int64, error) {
+	file, err := os.Stat(filepath.Join("blobs", digest))
+	if err != nil {
+		return 0, fmt.Errorf("digest %s not found", digest)
+	}
+
+	return file.Size(), nil
+}
 
 // Handlers
 
@@ -18,7 +30,19 @@ func handshake(w http.ResponseWriter, r *http.Request) {
 }
 
 func getBlobByDigest(w http.ResponseWriter, r *http.Request) {
-	// Search /blobs/ directory for the digest
+	digest := r.PathValue("digest")
+
+	size, err := getBlob(digest)
+	if err != nil {
+		w.WriteHeader(404)
+		return
+	}
+
+	w.Header().Set("Content-Length", strconv.Itoa(int(size)))
+	w.Header().Set("Docker-Content-Digest", digest)
+	w.WriteHeader(200)
+
+	return
 }
 
 // https://github.com/opencontainers/distribution-spec/blob/main/spec.md
